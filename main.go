7eba6117663b85d80c81ef07b252f5fa4e77d4c9
Gildas Chabot <gildas.chabot@schibsted.com>
2018-03-01 17:10:18 +0100

    add skeleton

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var input *os.File
+var output *os.File
+
+var A int
+var B int
+var C int
+
+func solve() {
+	fmt.Fprintf(output, "%d\n", C)
+}
+
+func main() {
+	sample := os.Args[1]
+	fileIn := sample + ".in"
+	fileOut := sample + ".out"
+
+	var err error
+	input, err = os.Open(fileIn)
+	if err != nil {
+		panic(fmt.Sprintf("open %s: %v", fileIn, err))
+	}
+	output, err = os.Create(fileOut)
+	if err != nil {
+		panic(fmt.Sprintf("creating %s: %v", fileOut, err))
+	}
+	defer input.Close()
+	defer output.Close()
+
+	// Global
+	A = readInt()
+	B = readInt()
+	C = readInt()
+
+	solve()
+}
+
+func readInt() int {
+	var i int
+	fmt.Fscanf(input, "%d", &i)
+	return i
+}
+
+func readString() string {
+	var str string
+	fmt.Fscanf(input, "%s", &str)
+	return str
+}
+
+func readFloat() float64 {
+	var x float64
+	fmt.Fscanf(input, "%f", &x)
+	return x
+}
